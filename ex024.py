2e8f6e76b16f2a74b9d76d498c0ddede9e3715a3
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2020-12-15 11:55:08 -0300

    Implementado o exercício 024.

--- ex024.py
+++ ex024.py
@@ -1,1 +1,11 @@
-cidade = print('')
+#verificando as primeiras letras de um texto
+
+#Primeira forma e fazer
+cidade = str(input('Em que cidade você nasceu? '))
+cidade_maiuscula = cidade.upper()
+cidade_div = cidade_maiuscula.strip().split()
+print('SANTO' in cidade_div[0])
+
+#Segunda maneira de fazer
+cidade = str(input('Em que cidade você nasceu? ')).strip()
+print((cidade[:5].upper()) == 'SANTO')
