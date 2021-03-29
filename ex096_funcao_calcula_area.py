26a01b721b1f71be444c73c025d8280f199181f1
Fernando <luiz.fernando_1990@hotmail.com>
2021-03-29 10:55:47 -0300

    Resolução do exercício ex097.

--- ex096_funcao_calcula_area.py
+++ ex096_funcao_calcula_area.py
@@ -4,14 +4,16 @@ def calculo_area():
     area = largura * comprimento
     print(f'A área de um terreno com a lagura de {largura} metros e o comprimento de {comprimento} metros é '
           f'{area:.2f} metros quadrados.')
-def espaçador():
+
+
+def esp():
     print('-' * 120)
 
 
-espaçador()
+esp()
 print('Controle de terrenos')
-espaçador()
+esp()
 largura = float(input('Lagura (em metros): '))
 comprimento = float(input('Comprimento (em metros): '))
 calculo_area()
-espaçador()
+esp()
---
+++ ex097_um_print_especial.py
@@ -0,0 +1,15 @@
+"Ex097 Um print especial"
+
+def escreva(txt):
+
+    tamanho = len(txt) + 4
+    print('-' * (tamanho))
+    print(f'  {txt}')
+    print('-' * (tamanho))
+
+
+
+
+escreva('Fernando')
+escreva('Curso')
+escreva('Python')
